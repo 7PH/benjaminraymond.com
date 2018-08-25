1fb96a8462025bde59b4742ac603d724f14e189c
PowerHat <b.raymond@protonmail.com>
2018-08-25 19:11:45 +0200

    Add page preview

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,15 +2,25 @@ import {Stage} from "./animation/engine/Stage";
 import {NodeContainer} from "./animation/nodes/NodeContainer";
 import AudioHandler from "./audio/AudioHandler";
 
-window.onload = () => {
+async function start() {
+
+    let pagePreview: HTMLElement | null = document.getElementById('page-preview');
+    let pageContent: HTMLElement | null = document.getElementById('page-content');
+
+    if (pagePreview === null || pageContent === null)
+        return alert("Error. Please try to refresh the page");
+
+    pagePreview.style.display = 'none';
+    pageContent.style.display = 'block';
+
+    // animation
     const stage: Stage = new Stage('animation-canvas');
-
-    let nodeContainer = new NodeContainer(stage);
+    const nodeContainer: NodeContainer = new NodeContainer(stage);
     nodeContainer.populate();
-
     stage.addChild(nodeContainer);
     stage.run();
 
+    // fps counter
     setInterval(() => {
         const fps = (1 / stage.lastDelta);
         let el = document.getElementById('fps');
@@ -19,7 +29,12 @@ window.onload = () => {
         el.innerHTML = Math.floor(fps).toString();
     }, 100);
 
-    AudioHandler.init('assets/crossfire.webm');
-};
+    // start sound
+    setTimeout(async () => {
 
-document.addEventListener('click', async () => AudioHandler.play());
+        await AudioHandler.play();
+    }, 400);
+}
+
+AudioHandler.init('assets/crossfire.webm');
+document.addEventListener('click', start);
