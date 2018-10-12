4566504c10f6e843bccb570fb458649d0a6ddc74
PowerHat <b.raymond@protonmail.com>
2018-10-12 06:08:45 +0100

    Remove blur effect on nodes

--- app/src/animation/nodes/NodeContainer.ts
+++ app/src/animation/nodes/NodeContainer.ts
@@ -2,8 +2,6 @@ import {DisplayObjectContainer} from "../engine/DisplayObjectContainer";
 import {Stage} from "../engine/Stage";
 import {Node} from "./Node";
 import {NodeLinker} from "./NodeLinker";
-import BlurFilter = PIXI.filters.BlurFilter;
-import AudioHandler from "../../audio/AudioHandler";
 
 /**
  *
@@ -12,14 +10,10 @@ export class NodeContainer extends DisplayObjectContainer {
 
     public nodes: DisplayObjectContainer;
 
-    public readonly blurFilter: BlurFilter;
-
     constructor (stage: Stage) {
         super(stage);
 
         this.nodes = new DisplayObjectContainer(this.stage);
-        this.blurFilter = new PIXI.filters.BlurFilter();
-        this.nodes.filters = [this.blurFilter];
         this.addChild(this.nodes);
         this.addChildAt(new NodeLinker(this.stage, this.nodes), 0);
 
@@ -48,9 +42,4 @@ export class NodeContainer extends DisplayObjectContainer {
         this.nodes.addChild(node);
     }
 
-    public update(delta: number) {
-        super.update(delta);
-
-        this.blurFilter.blur = 10 * Math.exp(- 13 * AudioHandler.average);
-    }
 }
