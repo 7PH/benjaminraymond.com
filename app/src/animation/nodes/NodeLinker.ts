95cf0a4c7c2bfc09dd676134700e1e5f17be0bb8
PowerHat <b.raymond@protonmail.com>
2018-10-25 23:38:04 +0200

    Draw link between nodes close to center and center

--- app/src/animation/nodes/NodeLinker.ts
+++ app/src/animation/nodes/NodeLinker.ts
@@ -3,16 +3,50 @@ import {Point} from "../engine/Point";
 import {DisplayObjectContainer} from "../engine/DisplayObjectContainer";
 import {Stage} from "../engine/Stage";
 import {Node} from "./Node";
+import AudioHandler from "../../audio/AudioHandler";
 
 
 export class NodeLinker extends DisplayObject {
 
     private target: DisplayObjectContainer;
 
+    private readonly centerPosition: PIXI.Point;
+
     constructor (stage: Stage, nodes: DisplayObjectContainer) {
         super(stage);
 
         this.target = nodes;
+
+        this.centerPosition = new PIXI.Point(
+            this.stage.getWidth() / 2,
+            6 * this.stage.getHeight() / 10
+        );
+    }
+
+    /**
+     *
+     */
+    redraw() {
+
+        if (typeof this.graphics === 'undefined')
+            return;
+
+        this.graphics.clear();
+        this.graphics.lineStyle(1, 0xFFFFFF, 0.2);
+
+        const N: number = this.target.children.length;
+        for (let i = 0; i < N; ++ i) {
+
+            const node: Node = this.target.children[i] as Node;
+            const distance: number = Point.distance(node.position, this.centerPosition);
+            console.log(distance);
+            if (distance > this.stage.getWidth() / 10)
+                continue;
+
+            // node is close to the center
+            this.graphics.moveTo(node.position.x, node.position.y);
+            this.graphics.lineTo(this.centerPosition.x, this.centerPosition.y);
+        }
     }
 
     /**
@@ -29,12 +63,8 @@ export class NodeLinker extends DisplayObject {
         for (let i = 0; i < N; i++) {
             const node = this.target.children[i] as Node;
 
-            const targetPosition: PIXI.Point = new PIXI.Point(
-                this.stage.getWidth() / 2,
-                6 * this.stage.getHeight() / 10
-            );
-            const angle: number = Math.atan2(node.position.y - targetPosition.y, node.position.x - targetPosition.x);
-            const dist = Point.distance(node.position, targetPosition);
+            const angle: number = Math.atan2(node.position.y - this.centerPosition.y, node.position.x - this.centerPosition.x);
+            const dist = Point.distance(node.position, this.centerPosition);
             const fx: number = - Math.cos(angle) * 100000 / dist;
             const fy: number = - Math.sin(angle) * 100000 / dist;
             node.setForce('tocenter', {x: fx, y: fy});
@@ -68,5 +98,7 @@ export class NodeLinker extends DisplayObject {
                 otherNode.setForce('node_' + node.id, new PIXI.Point(fx, fy));
             }
         }
+
+        this.redraw();
     }
 }
