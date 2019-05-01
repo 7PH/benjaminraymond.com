a85036794377fa451e108f55f9bbf8ee298380ad
PowerHat <b.raymond@protonmail.com>
2019-05-01 18:39:40 +0200

    Remove debug console.log

--- app/src/animation/nodes/NodeLinker.ts
+++ app/src/animation/nodes/NodeLinker.ts
@@ -39,7 +39,6 @@ export class NodeLinker extends DisplayObject {
 
             const node: Node = this.target.children[i] as Node;
             const distance: number = Point.distance(node.position, this.centerPosition);
-            console.log(distance);
             if (distance > this.stage.getWidth() / 10)
                 continue;
 
