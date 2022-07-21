ddbf34954c0c0755a8dfc8ff73330d1b18ab41cc
7PH <b.raymond@protonmail.com>
2022-07-21 16:37:06 +0200

    Add PowerGlitch & Project icons

--- src/components/section/ProjectsSection.js
+++ src/components/section/ProjectsSection.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Parallax } from 'react-scroll-parallax';
-import { isMobile } from 'react-device-detect';
 import './ProjectsSection.scss';
 
 
@@ -32,6 +30,11 @@ class ProjectsSection extends React.Component {
                 <div className='w-full max-w-4xl mx-auto grid grid-cols-2 gap-4 items-center text-left'>
 
                     <div className="bg-slate-900 col-span-2 rounded-md px-5 py-4 flex flex-col md:flex-row">
+                        <img
+                            className='mr-4 w-8 h-8'
+                            alt="risibank-logo"
+                            src="/assets/projects/skychat.png"
+                        />
                         <p className="text-lg font-bold" style={{ minWidth: 140, }}>
                             SkyChat
                         </p>
@@ -51,6 +54,11 @@ class ProjectsSection extends React.Component {
                     </div>
 
                     <div className="bg-slate-900 col-span-2 rounded-md px-5 py-4 flex flex-col md:flex-row">
+                        <img
+                            className='mr-4 w-8 h-8'
+                            alt="risibank-logo"
+                            src="/assets/projects/risibank.png"
+                        />
                         <p className="text-lg font-bold" style={{ minWidth: 140, }}>
                             RisiBank
                         </p>
@@ -70,6 +78,35 @@ class ProjectsSection extends React.Component {
                     </div>
 
                     <div className="bg-slate-900 col-span-2 rounded-md px-5 py-4 flex flex-col md:flex-row">
+                        <img
+                            className='mr-4 w-8 h-8'
+                            alt="risibank-logo"
+                            src="/assets/projects/powerglitch.gif"
+                        />
+                        <p className="text-lg font-bold" style={{ minWidth: 140, }}>
+                            PowerGlitch
+                        </p>
+                        <p className='flex-1 text-lg'>
+                            Tiny JS library to glitch images on the web
+                        </p>
+                        <div className="text-right mt-2 md:mt-0" style={{ width: 120 }}>
+                            <a
+                                className="btn btn-magenta-crayola w-full"
+                                href="https://github.com/7PH/powerglitch"
+                                target='_blank'
+                                rel="noreferrer"
+                            >
+                                See project
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="bg-slate-900 col-span-2 rounded-md px-5 py-4 flex flex-col md:flex-row">
+                        <img
+                            className='mr-4 w-8 h-8'
+                            alt="risibank-logo"
+                            src="/assets/projects/poweraudio.png"
+                        />
                         <p className="text-lg font-bold" style={{ minWidth: 140, }}>
                             PowerAudio
                         </p>
@@ -78,7 +115,7 @@ class ProjectsSection extends React.Component {
                         </p>
                         <div className="text-right mt-2 md:mt-0" style={{ width: 120 }}>
                             <a
-                                className="btn btn-amethyst w-full"
+                                className="btn btn-capri w-full"
                                 href="https://github.com/7PH/poweraudio"
                                 target='_blank'
                                 rel="noreferrer"
